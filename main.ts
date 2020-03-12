ef83f24f28afb9bf527e64238bc1c4a56be4fd94
surasakBualiang <62088770+surasakBualiang@users.noreply.github.com>
2020-03-12 13:21:34 +0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+
+
+/**
+ * IR remote
+ */
+    //% color=50 weight=80
+    //% icon="\uf1eb"
+namespace IR {
+
+  /**
+  * initialization
+  */
+  //% blockId=ir_init
+  //% blockGap=20 weight=90
+  //% block="connect ir receiver to %pin"
+  //% shim=IR::init
+  export function init(pin: Pins): void {
+    return
+  }
+
+  /**
+  * button pushed.
+  */
+  //% blockId=ir_received_event
+  //% blockGap=20 weight=70
+  //% block="on |%btn| button pressed"
+  //% shim=IR::onPressEvent
+  export function onPressEvent(btn: RemoteButton, body:Action): void {
+    return
+  }
+
+}
